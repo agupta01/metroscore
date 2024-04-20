d0e92979086b30b49669d29e4cae5330bfec25e2
Arunav Gupta <centaurus.ab@gmail.com>
2024-04-20 10:45:30 -0400

    temp: driveshed train file

--- metroscore/estimator/train.py
+++ metroscore/estimator/train.py
@@ -2,7 +2,6 @@ import torch
 import networkx as nx
 import osmnx as ox
 
-def load_road_data(origin: str) -> nx.Graph:
+def load_road_data(origin: str) -> nx.classes.multidigraph.MultiDiGraph:
 	G = ox.graph_from_place(origin, network_type="drive")
 	return G
-
