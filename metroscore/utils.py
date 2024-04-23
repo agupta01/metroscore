85345ba69ee31ff9c6ec10f623fcca3265c4ef70
Arunav Gupta <centaurus.ab@gmail.com>
2024-04-23 09:37:13 -0400

    temp: graph merge

--- metroscore/utils.py
+++ metroscore/utils.py
@@ -1,8 +1,18 @@
-from shapely.geometry import Point, LineString
-from typing import Tuple, Dict
-from networkx import MultiDiGraph
-import osmnx as ox
 import warnings
+from functools import cache
+from typing import Dict, Tuple
+
+import networkx as nx
+import osmnx as ox
+from geopy.distance import distance, lonlat
+from networkx import Graph, MultiDiGraph
+from shapely.geometry import Point
+
+
+def __fill_missing_geometries(graph: Graph) -> Graph:
+    """Fill missing edge geometries in graph using the geometry of the nodes."""
+    n, e = ox.graph_to_gdfs(graph, nodes=True, edges=True, fill_edge_geometry=True)
+    return ox.graph_from_gdfs(n, e)
 
 
 def project_node_to_edge(node: Dict, edge: Tuple) -> Point:
@@ -18,15 +28,15 @@ def project_node_to_edge(node: Dict, edge: Tuple) -> Point:
     """
     node_geom = Point(node["x"], node["y"])
     edge_geom = edge[2]["geometry"]
-    interpolate_dist = edge_geom.project(node)
+    interpolate_dist = edge_geom.project(node_geom)
     return edge_geom.interpolate(interpolate_dist)
 
 
-def get_node_to_node_mapping(a: MultiDiGraph, b: MultiDiGraph) -> Dict[int, Tuple[int, int]]:
-    """Returns which nodes in graph b are closest to each node in graph a, along with their distances."""
+def get_node_to_node_mapping(a: Graph, b: Graph) -> Dict[int, int]:
+    """Returns which nodes in graph b are closest to each node in graph a."""
     # project graph if not done so
-    a = ox.projection.project_graph(a, to_latlong=True)
-    b = ox.projection.project_graph(b, to_latlong=True)
+    a = ox.project_graph(a, to_latlong=True)
+    b = ox.project_graph(b, to_latlong=True)
     Xs = []
     Ys = []
     for _id, node in a.nodes(data=True):
@@ -39,8 +49,8 @@ def get_node_to_node_mapping(a: MultiDiGraph, b: MultiDiGraph) -> Dict[int, Tupl
             )
         Xs.append(node["x"])
         Ys.append(node["y"])
-    nn, dist = ox.distance.nearest_nodes(G=b, X=Xs, Y=Ys, return_dist=True)
-    return dict(zip(a.nodes, zip(nn, dist)))
+    nn = ox.nearest_nodes(G=b, X=Xs, Y=Ys)
+    return dict(zip(a.nodes, nn))
 
 
 def merge_node_to_node(a: Tuple[int, Dict], b: Tuple[int, Dict]) -> Tuple[int, Dict]:
@@ -48,4 +58,47 @@ def merge_node_to_node(a: Tuple[int, Dict], b: Tuple[int, Dict]) -> Tuple[int, D
     new_x = (a[1]["x"] + b[1]["x"]) / 2
     new_y = (a[1]["y"] + b[1]["y"]) / 2
     # keep a's node id
-    return a[0], {"x": new_x, "y": new_y, "lon": new_x, "lat": new_y}
+    return a[0], {"x": new_x, "y": new_y, "lon": new_x, "lat": new_y, "merge_provenance": b[0]}
+
+
+@cache
+def __prep_node_tuple(graph: Graph, node_id: int) -> Tuple[int, Dict]:
+    return node_id, graph.nodes(data=True)[node_id]
+
+
+def merge_graphs(a: MultiDiGraph, other: MultiDiGraph, tol: float = 20.0) -> Graph:
+    """Merge `other` into `a` by connecting nodes that are within `tol` meters of each other.
+    Also merge the edges.
+
+    Args:
+        a: MultiDiGraph
+        other: MultiDiGraph
+        tol: float, default 20.
+            Tolerance in meters for merging nodes.
+
+    Returns:
+        MultiDiGraph: Merged graph.
+    """
+    # make a copy of a
+    G = a.copy()
+    G_x = nx.get_node_attributes(G, "x")
+    G_y = nx.get_node_attributes(G, "y")
+    O_x = nx.get_node_attributes(other, "x")
+    O_y = nx.get_node_attributes(other, "y")
+    # get node to node mapping for other
+    mapping = get_node_to_node_mapping(a=G, b=other)
+    # merge nodes in other that are within tol of a
+    new_node_attributes = {}
+    for n1, n2 in mapping.items():
+        dist = distance(lonlat(G_x[n1], G_y[n1]), lonlat(O_x[n2], O_y[n2])).m
+        if dist < tol:
+            # print(f"Merging nodes {n1} and {n2} at distance {dist:.2f}m")
+            node_id, node_attributes = merge_node_to_node(
+                a=__prep_node_tuple(G, n1), b=__prep_node_tuple(other, n2)
+            )
+            new_node_attributes[node_id] = node_attributes
+        else:
+            # print(f"Nodes {n1} and {n2} are too far apart at distance {dist:.2f}m")
+            pass
+    nx.set_node_attributes(G, new_node_attributes)
+    return G
