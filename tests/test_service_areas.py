60c5be213500725c6e0a7b8ae059fda140fe3cb6
Jiali "Tony" Qian <qjl991026@gmail.com>
2024-07-26 10:03:34 +0800

    test: add tests for A* implementation

--- tests/test_service_areas.py
+++ tests/test_service_areas.py
@@ -6,6 +6,7 @@ from metroscore.service_areas import (
     floyd_warshall_fast,
     floyd_warshall_slow,
     time_dependent_dijkstra,
+    time_dependent_a_star
 )
 
 
@@ -57,6 +58,30 @@ def test_tdsp_djikstra():
     assert result[3] == 13
     assert result[4] == 18
 
+def test_tdsp_a_star():
+    G = nx.DiGraph()
+    G.add_edges_from(
+        [
+            (1, 2, {"travel_time": 6}),
+            (1, 3, {"travel_time": 10}),
+            (2, 3, {"travel_time": 3}),
+            (3, 4, {"travel_time": 5}),
+            (2, 4, {"travel_time": 12}),
+        ]
+    )
+    every_second = list(range(86400))
+    timetable = pd.Series(
+        data=[every_second] * 5,
+        index=pd.MultiIndex.from_tuples([(1, 2), (1, 3), (2, 3), (3, 4), (2, 4)]),
+    )
+    start_time = 4
+    origin_id = 1
+    result = time_dependent_a_star(G, timetable, start_time, origin_id)
+
+    assert result[1] == 4
+    assert result[2] == 10
+    assert result[3] == 13
+    assert result[4] == 18
 
 def test_tdsp_djikstra_every_other_second():
     """Departure times at all nodes are every other second."""
