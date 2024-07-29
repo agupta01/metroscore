cbf2fed5806ab9336560838838969305ca7b2ebd
Jiali "Tony" Qian <48393722+DSC-Qian@users.noreply.github.com>
2024-07-30 06:34:48 +0800

    feat: implement A* with zero heuristics (#67)

--- tests/test_service_areas.py
+++ tests/test_service_areas.py
@@ -5,6 +5,7 @@ import pandas as pd
 from metroscore.service_areas import (
     floyd_warshall_fast,
     floyd_warshall_slow,
+    time_dependent_a_star,
     time_dependent_dijkstra,
 )
 
@@ -51,6 +52,32 @@ def test_tdsp_djikstra():
     start_time = 4
     origin_id = 1
     result = time_dependent_dijkstra(G, timetable, start_time, origin_id)
+
+    assert result[1] == 4
+    assert result[2] == 10
+    assert result[3] == 13
+    assert result[4] == 18
+
+
+def test_tdsp_a_star():
+    G = nx.DiGraph()
+    G.add_edges_from(
+        [
+            (1, 2, {"travel_time": 6}),
+            (1, 3, {"travel_time": 10}),
+            (2, 3, {"travel_time": 3}),
+            (3, 4, {"travel_time": 5}),
+            (2, 4, {"travel_time": 12}),
+        ]
+    )
+    every_second = list(range(86400))
+    timetable = pd.Series(
+        data=[every_second] * 5,
+        index=pd.MultiIndex.from_tuples([(1, 2), (1, 3), (2, 3), (3, 4), (2, 4)]),
+    )
+    start_time = 4
+    origin_id = 1
+    result = time_dependent_a_star(G, timetable, start_time, origin_id)
 
     assert result[1] == 4
     assert result[2] == 10
